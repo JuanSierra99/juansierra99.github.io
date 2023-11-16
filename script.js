c4bce761ee25eea9a084073b34dfae6b9f8b7adc
Juan Sierra <sierra.juan99@gmail.com>
2023-11-16 01:30:11 -0800

    Created a script
    Only function is to type text

---
+++ script.js
@@ -0,0 +1,39 @@
+const typing = async () => {
+  let docElement = document.getElementById("imTyping");
+  const string1 = "Hello, World !";
+  const string2 = " i'm Juan Sierra";
+
+  const constructString = (str) => {
+    for (let i in str) {
+      setTimeout(
+        (index) => {
+          docElement.textContent += str[index];
+          if (str[index] == "!") {
+            setTimeout(() => {
+              deconstructString();
+            }, 900);
+          }
+        },
+        i * 110,
+        i
+      );
+    }
+    resolve();
+  };
+
+  const deconstructString = async () => {
+    let interval = setInterval(() => {
+      let lastChar = docElement.textContent.charAt(
+        docElement.textContent.length - 1
+      );
+      if (lastChar !== ",") {
+        docElement.textContent = docElement.textContent.slice(0, -1);
+      } else {
+        clearInterval(interval); // Stop deconstruction at the comma
+        constructString(string2); // Construct second string
+      }
+    }, 100);
+  };
+  constructString(string1);
+};
+typing();
