c6f2d8b813cc0a39701d5d93c2089170bb7549fb
Juan Sierra <sierra.juan99@gmail.com>
2024-01-09 00:16:37 -0800

    should help improve page performance

--- script.js
+++ script.js
@@ -1,47 +1,49 @@
-const typing = async () => {
-  let docElement = document.getElementById("imTyping");
-  const string1 = "Hello, World !";
-  const string2 = " I'm Juan Sierra :)";
+// const typing = async () => {
+// let docElement = document.getElementById("imTyping");
+// const string1 = "Hello, World !";
+// const string2 = " I'm Juan Sierra :)";
 
-  const constructString = (str) => {
-    for (let i in str) {
-      setTimeout(
-        (index) => {
-          docElement.textContent += str[index];
-          if (str[index] == "!") {
-            setTimeout(() => {
-              deconstructString();
-            }, 900);
-          }
-        },
-        i * 110,
-        i
-      );
-    }
-    resolve();
-  };
+//   const constructString = (str) => {
+//     for (let i in str) {
+//       setTimeout(
+//         (index) => {
+//           docElement.textContent += str[index];
+//           if (str[index] == "!") {
+//             setTimeout(() => {
+//               deconstructString();
+//             }, 900);
+//           }
+//         },
+//         i * 110,
+//         i
+//       );
+//     }
+//     resolve();
+//   };
 
-  const deconstructString = async () => {
-    let interval = setInterval(() => {
-      let lastChar = docElement.textContent.charAt(
-        docElement.textContent.length - 1
-      );
-      if (lastChar !== ",") {
-        docElement.textContent = docElement.textContent.slice(0, -1);
-      } else {
-        clearInterval(interval); // Stop deconstruction at the comma
-        constructString(string2); // Construct second string
-      }
-    }, 100);
-  };
-  constructString(string1);
-};
-typing();
+//   const deconstructString = async () => {
+//     let interval = setInterval(() => {
+//       let lastChar = docElement.textContent.charAt(
+//         docElement.textContent.length - 1
+//       );
+//       if (lastChar !== ",") {
+//         docElement.textContent = docElement.textContent.slice(0, -1);
+//       } else {
+//         clearInterval(interval); // Stop deconstruction at the comma
+//         constructString(string2); // Construct second string
+//       }
+//     }, 100);
+//   };
+//   constructString(string1);
+// };
+// typing();
 
 const observer = new IntersectionObserver((target) => {
   target.forEach((t) => {
     if (t.isIntersecting && t.target.complete) {
+      console.log("hereee");
       t.target.classList.add("show");
+      observer.unobserve(t.target);
     }
     //  else {
     //   t.target.classList.remove("show");
